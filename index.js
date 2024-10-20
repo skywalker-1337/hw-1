44fe1cc42461da8e25be7a32cf4a0dd6be335a45
skywalker-1337 <kobalianikolaj@gmail.com>
2024-10-20 23:44:28 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,56 @@
+// Задание 1
+let a = 10;
+alert(a);
+a = 20;
+alert(a);
+
+// Задание 2
+let releaseYear = 2007;
+alert(releaseYear);
+
+// Задание 3
+let creatorName = "Brendan Eich";
+alert(creatorName);
+
+// Задание 4
+let numberOne = 10;
+let numberTwo = 2;
+
+alert("Сумма:" + (numberOne + numberTwo));
+alert("Разнсоть:" + (numberOne - numberTwo));
+alert("Произведение:" + (numberOne * numberTwo));
+alert("Чатсное:" + (numberOne / numberTwo));
+
+// Задание 5
+let result = 2 ** 5;
+alert(result);
+
+// Задание 6, изменил а на другую букву, так как иначе ошибку показывает
+let c = 9;
+let b = 2;
+alert(c % b);
+
+// Задание 7
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num++;
+num--;
+alert(num);
+
+// Задание 8
+let age = prompt();
+alert(age);
+
+// Задание 9
+let user = {
+    name: "Niko",
+    age: 19,
+    isAdmin: true
+}
+
+// Задание 10
+let userName = prompt("Как тебя зовут?");
+alert(`Привет, ${userName}!`);
